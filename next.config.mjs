2e6a3247b76799b9fc646e9b82e786e5d8862946
abdelaziz-sekouti <abdelaziz-sekouti@hotmail.com>
2025-11-21 15:33:07 +0100

    fix script command

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: '/next-app',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
